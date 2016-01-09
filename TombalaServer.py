8bedb689621fdecf0033391f2615d7ad0065d703
ozlemakbas <ozlemakbas90@gmail.com>
2016-01-09 22:16:33 +0200

    Basic server sample code added

--- TombalaServer.py
+++ TombalaServer.py
@@ -6,4 +6,18 @@ import sys
 import socket
 import threading
 import Queue
-import ime
+
+#Main Thread
+
+ssoc = socket.socket()
+host = socket.gethostname()
+print host
+port = 12345
+ssoc.bind((host, port))
+ssoc.listen(5)
+while True:
+    c, addr = ssoc.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    c.send('Thank you for connecting!')
+    c.close()
+
